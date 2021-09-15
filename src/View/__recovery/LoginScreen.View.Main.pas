4590216594da8c1c11a8918b5fc851ac09c23c5a
ryuuzera <87572712+ryuuzera@users.noreply.github.com>
2021-09-15 00:56:00 -0300

    update

---
+++ src/View/__recovery/LoginScreen.View.Main.pas
@@ -0,0 +1,159 @@
+unit LoginScreen.View.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg, Vcl.Buttons, Vcl.Imaging.pngimage,  LoginScreen.View.Styles.Colors;
+
+type
+  TfrmLogin = class(TForm)
+    CenterPanel: TPanel;
+    Image1: TImage;
+    shpBottom: TShape;
+    shpCenter: TShape;
+    shpTop: TShape;
+    Image3: TImage;
+    shpButton: TShape;
+    edLogin: TEdit;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Image2: TImage;
+    Image4: TImage;
+    Label2: TLabel;
+    Panel4: TPanel;
+    Image5: TImage;
+    Image6: TImage;
+    Panel6: TPanel;
+    Image8: TImage;
+    Shape1: TShape;
+    Shape2: TShape;
+    Shape3: TShape;
+    Shape4: TShape;
+    Panel5: TPanel;
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure SpeedButton2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure applyStyles;
+    procedure shpButtonMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure shpButtonMouseLeave(Sender: TObject);
+    procedure CenterPanelMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Panel1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Panel4Click(Sender: TObject);
+    procedure Image8Click(Sender: TObject);
+    procedure Image7Click(Sender: TObject);
+    procedure Panel5Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    Styles : TStyles;
+    procedure roundForm;
+    { Public declarations }
+  end;
+
+var
+  frmLogin: TfrmLogin;
+
+implementation
+
+uses
+  Vcl.Dialogs, LoginScreen.View.PDV ;
+
+{$R *.dfm}
+
+procedure TfrmLogin.applyStyles;
+begin
+  Styles := TStyles.Create;
+  Panel2.Color := Styles.PANELCOLOR;
+  Panel3.Color := Styles.PANELCOLOR;
+  shpButton.Brush.Color := Styles.PANELCOLOR;
+  shpButton.Pen.Color := Styles.PANELCOLOR;
+  Label1.Font.Size := Styles.H3;
+end;
+
+procedure TfrmLogin.CenterPanelMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+const
+  sc_DragMove = $F012;
+begin
+  ReleaseCapture;
+  Perform(wm_SysCommand, sc_DragMove, 0);
+end;
+
+procedure TfrmLogin.FormCreate(Sender: TObject);
+begin
+  roundForm;
+  applyStyles;
+end;
+
+procedure TfrmLogin.Image7Click(Sender: TObject);
+begin
+  frmLogin.WindowState := wsMaximized;
+end;
+
+procedure TfrmLogin.Image8Click(Sender: TObject);
+begin
+  Application.Minimize;
+end;
+
+procedure TfrmLogin.Panel1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+const
+  sc_DragMove = $F012;
+begin
+  ReleaseCapture;
+  Perform(wm_SysCommand, sc_DragMove, 0);
+end;
+
+procedure TfrmLogin.Panel4Click(Sender: TObject);
+begin
+ frmLogin.Close;
+end;
+
+procedure TfrmLogin.Panel5Click(Sender: TObject);
+begin
+  Self.Hide;
+  PDV.Show;
+end;
+
+procedure TfrmLogin.roundForm;
+  var
+  rgn: HRGN;
+begin
+  frmLogin.DoubleBuffered := True;
+  rgn := CreateRoundRectRgn(0, 0,ClientWidth,ClientHeight,12,12);
+  SetWindowRgn(Handle, rgn, True);
+end;
+
+procedure TfrmLogin.shpButtonMouseLeave(Sender: TObject);
+begin
+  shpButton.Brush.Color := Styles.PANELCOLOR;
+  shpButton.Pen.Color := Styles.PANELCOLOR;
+end;
+
+procedure TfrmLogin.shpButtonMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  shpButton.Brush.Color := Styles.MOVE_BUTTON_COLOR;
+  shpButton.Pen.Color := Styles.MOVE_BUTTON_COLOR;
+end;
+
+procedure TfrmLogin.SpeedButton1Click(Sender: TObject);
+begin
+  frmLogin.Close;
+end;
+
+procedure TfrmLogin.SpeedButton2Click(Sender: TObject);
+begin
+  Application.Minimize;
+end;
+
+end.
